ef5e498bc512899ed3ee039641e667667925e9c4
CalumBarr227 <calumbarr227@gmail.com>
2026-08-24 16:35:18 +0100

    Ball now behaves same as in actual Pong

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -5,26 +5,46 @@ using UnityEngine;
 public class Ball : MonoBehaviour
 {
     public float speed = 5f;
+    Rigidbody2D rb;
 
     void Start()
     {
+        rb = GetComponent<Rigidbody2D>();
         int direction = Random.Range(0, 2);
 
         if (direction == 0)
         {
-            speed = -speed;
+            rb.velocity = new Vector2(-speed, Random.Range(-2f, 2f));
         }
-
-        //GetComponent<Rigidbody2D>().velocity = new Vector2(speed, 0);
-        GetComponent<Rigidbody2D>().velocity = new Vector2(speed, 2);
-
-
+        else
+        {
+            rb.velocity = new Vector2(speed, Random.Range(-2f, 2f));
+        }
     }
+            //GetComponent<Rigidbody2D>().velocity = new Vector2(speed, 0);
+            //GetComponent<Rigidbody2D>().velocity = new Vector2(speed, 2);
 
     public void ResetBall()
     {
         GetComponent<Rigidbody2D>().velocity = Vector2.zero;
         transform.position = Vector2.zero;
         Invoke("Start", 1f);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Paddle"))
+        {
+            float difference = transform.position.y - collision.transform.position.y;
+
+            if(collision.transform.position.x > 0)
+            {
+                rb.velocity = new Vector2(-speed, difference * 3);
+            }
+            else
+            {
+                rb.velocity = new Vector2(speed, difference * 3);
+            }
+        }
     }
 }
